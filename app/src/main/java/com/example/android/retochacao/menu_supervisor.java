16c39f6b0d37284b86c640d52b8600dc573f5e5d
Miguel Saraiva <migsar282@gmail.com>
2015-07-10 09:05:20 -0430

    Modificadas varias pantallas para linkear funcionalidad, creado menu temporal del supervisor

--- app/src/main/java/com/example/android/retochacao/menu_supervisor.java
+++ app/src/main/java/com/example/android/retochacao/menu_supervisor.java
@@ -6,21 +6,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 
-public class Login_Supervisor extends ActionBarActivity {
+public class menu_supervisor extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login__supervisor);
+        setContentView(R.layout.activity_menu_supervisor);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login__supervisor, menu);
+        getMenuInflater().inflate(R.menu.menu_menu_supervisor, menu);
         return true;
     }
 
@@ -39,22 +38,9 @@ public class Login_Supervisor extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void limpiarDatos (View view) {
-        EditText usuario = (EditText) findViewById(R.id.Usuario);
-        usuario.setText("");
-        EditText contraseña = (EditText) findViewById(R.id.Contraseña);
-        contraseña.setText("");
+    public void verFuncionarios (View view){
+        Intent nextScreen = new Intent(getApplicationContext(), Login_Supervisor.class);
+        startActivity(nextScreen);
     }
 
-    public void verFuncionarios (View view) {
-        EditText editText = (EditText) findViewById(R.id.Usuario);
-        String texto = editText.getText().toString();
-        if (texto.equals("admin")) {
-            Intent nextScreen = new Intent(getApplicationContext(), funcionarios.class);
-            startActivity(nextScreen);
-        } else {
-            Intent nextScreen = new Intent(getApplicationContext(), Login_Supervisor.class);
-            startActivity(nextScreen);
-        }
-    }
 }
